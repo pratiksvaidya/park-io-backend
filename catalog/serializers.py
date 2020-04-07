23de812c24195034b48dd5d02b00a857c0cd7ca4
Pratik Vaidya <psvaidya@umich.edu>
2020-04-07 07:32:57 -0700

    add url

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -5,5 +5,5 @@ from rest_framework import serializers
 class ParkingSpotSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ParkingSpot
-        fields = ['address', 'rate', 'contact', 'description', 'occupied', 'lat', 'lon']
+        fields = ['address', 'rate', 'contact', 'description', 'occupied', 'lat', 'lon', 'url']
         read_only = ['lat', 'lon']
